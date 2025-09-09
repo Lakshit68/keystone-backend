dc87ea5e5ef61417bbdea13a1fcfed1c16ee863c
Lakshit <Lakshitsachdeva25@gmail.com>
2025-09-09 12:45:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/keystone-admin', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
